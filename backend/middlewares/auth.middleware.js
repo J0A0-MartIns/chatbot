aca52d09e3c578ae24003f084e06ffa10786e5d3
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-06-20 10:27:57 -0300

    Alteração auth

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -5,8 +5,7 @@ module.exports = (req, res, next) => {
     if (!authHeader) return res.sendStatus(401);
     const token = authHeader.split(' ')[1];
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded;
+        req.user = jwt.verify(token, process.env.JWT_SECRET);
         next();
     } catch {
         res.sendStatus(403);
