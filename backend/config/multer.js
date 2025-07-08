06519e368b4329ac79586a559f03f13c5d7b15bb
João Marcos Martins <joaomarcosmartins217@gmail.com>
2025-07-08 10:52:57 -0300

    Adicionando multer para uploads de arquivos

---
+++ backend/config/multer.js
@@ -0,0 +1,23 @@
+/**
+ * Configuração do middleware Multer para o upload de arquivos.
+ */
+const multer = require('multer');
+const path = require('path');
+
+// Define a estratégia de armazenamento dos ficheiros
+const armazenamento = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './uploads/docs/');
+    },
+    // Define como os arquivos serão nomeados para evitar conflitos
+    filename: (req, file, cb) => {
+        // Cria um nome de arquivo único usando a data atual e o nome original
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+});
+
+// Cria a instância do multer com a configuração de armazenamento
+const upload = multer({ storage: armazenamento });
+
+module.exports = upload;
